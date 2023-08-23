4281eebad6dcfeb6c302dae39b9957b0c81ac197
Драган Ћајић <cajic_dragan@yahoo.com>
2023-08-23 21:22:19 +0200

    ~ 01-16 `Lists` in Python ~
    > BEST PRACTICE:
    > You want to keep data inside a list homogeneous, so of the same kind, because
    > the data inside the list is no longer described by the variable name, e.g.:
    friends = ["Nebojsa", 2, "Momcilo"]  # bad practice, highly discouraged!!! :raised_eyebrow: :thumbsdown:
    
    > this is much better :grinning: :thumbsup:
    friends = ["Nebojsa", "Momcilo"]
    
    > or like this (if you need friend-age info!) :wink: :smiley:
    
    > list inside list (list of friends and their ages) :heavy_check_mark:
    friends = [["Nebojsa", 25], ["Momcilo", 30]]

---
+++ ch01_introduction/01-16_lists-in-python.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 23 07:17:06 2023
+
+@author: Драган Ћајић
+
+~ 01-16 Lists in Python ~
+-------------------------
+"""
+# BEST PRACTICE:
+# You want to keep data inside a list homogeneous, so of the same kind, because
+# the data inside the list is no longer described by the variable name, e.g.:
+friends = ["Nebojsa", 2, "Momcilo"]  # bad practice, highly discouraged!!!
+
+friends = ["Nebojsa", "Momcilo"]
+print(len(friends))
+
+# list inside list (list of friends and their ages)
+friends = [["Nebojsa", 25], ["Momcilo", 30]]
+print(friends[0][0])
+print(friends[1][1])
+
+# a long list of lists written a little bit prettier
+friends = [
+    ["Nebojsa", 25],
+    ["Momcilo", 30],
+    ["Vladimir", 35],
+]
+
+friends = ["Nebojsa", "Momcilo"]
+friends.append("Vladimir")
+print(friends)
+
+friends.remove("Nebojsa")
+print(friends)
