5e002ab55a391409b8bb8af8a1f164c4630ca53a
Драган Ћајић <cajic_dragan@yahoo.com>
2023-08-24 21:49:24 +0200

    ~ 01-20 `Exercise`: Nearby Friends (`Python 3.10`) ~
    $ NOTE:
      We `don't "append"` to sets because appending means "adding at the end".
      Since `sets don't have order`, there is no "end" to add to! :wink:
    
    > AttributeError: 'set' object has no attribute 'append' <-- LOOK!
    $ art_friends.append("Vladimir")  # ERROR !!!
    
    > So, instead of `append`, there is method called `add` for adding element
      to the set [duplicate element is NOT added to that set!]. :electric_plug: :brain:
    $ art_friends.add("Vladimir")

---
+++ ch01_introduction/01-20_exercise-nearby-friends.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 24 18:42:47 2023
+
+@author: Драган Ћајић
+
+~ Exercise: Nearby Friends (Python 3.10) ~
+------------------------------------------
+"""
+nearby_people = {'Rolf', 'Jen', 'Anna'}
+user_friends = set()  # This is an empty set, like {}
+
+# Ask the user for the name of a friend
+name_of_friend = input("What's your friend's name: ")
+
+# Add the name to the empty set
+user_friends.add(name_of_friend)
+
+# Print out the intersection between both sets.
+# This gives us a set with those friends that are nearby.
+print(nearby_people.intersection(user_friends))
