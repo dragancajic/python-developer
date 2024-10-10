49c07b2fac31827755a127c1aae1c93f010bb48a
Драган Ћајић <cajic_dragan@yahoo.com>
2024-10-10 23:54:35 +0200

    ~ `Unit Testing`: Testing `functions`
    › Trunk Code Quality -> Trunk Check `VSCode Extension`
    Automated Code Quality for Teams: universal formatting, linting, static analysis, and security
    
    › PyCharm 2022.2.2 (Edu)
    Runtime version: 17.0.4.1+7-b469.62 amd64
    
    `Toggle Full Screen Mode` | (`Alt+F`)

--- ch20_unit_testing/code/1_functions/functions.py
+++ ch20_unit_testing/code/1_functions/functions.py
@@ -1,15 +1,21 @@
-from typing import Union, Tuple
+"""
+    Testing functions
+"""
+from typing import Tuple, Union
+
 
 def divide(dividend: Union[int, float], divisor: Union[int, float]):
+    """divide function"""
     if divisor == 0:
-        raise ValueError('The divisor cannot be zero.')
-    
+        raise ValueError("The divisor cannot be zero.")
+
     return dividend / divisor
 
 
 def multiply(*args: Tuple[Union[int, float]]):
+    """multiply function"""
     if len(args) == 0:
-        raise ValueError('At least one value to multiply must be passed.')
+        raise ValueError("At least one value to multiply must be passed.")
     total = 1
     for arg in args:
         total *= arg
--- ch20_unit_testing/code/1_functions/test_functions.py
+++ ch20_unit_testing/code/1_functions/test_functions.py
@@ -1,58 +1,68 @@
-from functions import divide, multiply
+"""
+    Testing functions
+"""
+
 from unittest import TestCase
+
+from functions import divide
 
 
 class TestFunctions(TestCase):
+    """TestFunctions"""
+
     def test_divide_result(self):
+        """test equality: positive"""
         dividend = 15
         divisor = 3
         expected_result = 5.0
         self.assertAlmostEqual(divide(dividend, divisor), expected_result, delta=0.0001)
 
     def test_divide_negative(self):
+        """test equality: negative"""
         dividend = 15
         divisor = -3
         expected_result = -5.0
         self.assertAlmostEqual(divide(dividend, divisor), expected_result, delta=0.0001)
 
     def test_divide_dividend_zero(self):
+        """test equality: zero"""
         dividend = 0
         divisor = 5
         expected_result = 0
         self.assertEqual(divide(dividend, divisor), expected_result)
 
-    def test_divide_error_on_zero(self):
-        with self.assertRaises(ValueError):
-            divide(25, 0)
+    # def test_divide_error_on_zero(self):
+    #     with self.assertRaises(ValueError):
+    #         divide(25, 0)
 
-    def test_multiply_empty(self):
-        with self.assertRaises(ValueError):
-            multiply()
+    # def test_multiply_empty(self):
+    #     with self.assertRaises(ValueError):
+    #         multiply()
 
-    def test_multiply_single_value(self):
-        expected = 15
-        self.assertEqual(multiply(expected), expected)
+    # def test_multiply_single_value(self):
+    #     expected = 15
+    #     self.assertEqual(multiply(expected), expected)
 
-    def test_multiply_zero(self):
-        expected = 0
-        self.assertEqual(multiply(expected), expected)
+    # def test_multiply_zero(self):
+    #     expected = 0
+    #     self.assertEqual(multiply(expected), expected)
 
-    def test_multiply_result(self):
-        inputs = (3, 5)
-        expected = 15
-        self.assertEqual(multiply(*inputs), expected)
+    # def test_multiply_result(self):
+    #     inputs = (3, 5)
+    #     expected = 15
+    #     self.assertEqual(multiply(*inputs), expected)
 
-    def test_multiply_results_with_zero(self):
-        inputs = (3, 5, 0)
-        expected = 0
-        self.assertEqual(multiply(*inputs), expected)
+    # def test_multiply_results_with_zero(self):
+    #     inputs = (3, 5, 0)
+    #     expected = 0
+    #     self.assertEqual(multiply(*inputs), expected)
 
-    def test_multiply_negative(self):
-        inputs = (3, -5, 2)
-        expected = -30
-        self.assertEqual(multiply(*inputs), expected)
+    # def test_multiply_negative(self):
+    #     inputs = (3, -5, 2)
+    #     expected = -30
+    #     self.assertEqual(multiply(*inputs), expected)
 
-    def test_multiply_floats(self):
-        inputs = (3.0, 2)
-        expected = 6.0
-        self.assertEqual(multiply(*inputs), expected)
+    # def test_multiply_floats(self):
+    #     inputs = (3.0, 2)
+    #     expected = 6.0
+    #     self.assertEqual(multiply(*inputs), expected)
